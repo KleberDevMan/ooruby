6f60c18fd372641d72faedc8aa77a9ae0185362e
KleberDevMen <klebersubcontas@gmail.com>
2018-10-24 11:13:23 -0300

    05. Generalizando comportamentos através do method_missing
    Esse método é chamado quando um metodo nao é encontrado.
    Dentro dele é possivel fazer generalizações bem auto nível de métodos e comportametos.

--- estoque.rb
+++ estoque.rb
@@ -10,46 +10,24 @@ class Estoque
         @livros.extend Contador
     end
 
-    
-    # conta a quantidade de ocorrencia de uma valor em um array de string
-    def quantidade_de_vendas_por(produto, &campo)
-        @vendas.count {|venda| campo.call(venda) == campo.call(produto)} 
-    end
-    
-    # reorganiza o array de livro e retorna o ultimo
-    def livro_que_mais_vendeu_por(tipo, &campo)
-        @vendas.select {|l| l.tipo == tipo}.sort {|produto1,produto2|
-            quantidade_de_vendas_por(produto1, &campo) <=> 
-                quantidade_de_vendas_por(produto2, &campo)}.last
-    end
-    
-    # ---------- LIVRO -----------
+    # ---------- 'livro/revista/ebook'... que mais vendeu por... 'titulo/quantidade/editora' -----------
 
-    def livro_que_mais_vendeu_por_titulo
-        livro_que_mais_vendeu_por("livro", &:titulo)
+    # quando um metodo nao é encontrado
+    def method_missing(name)
+        matcher = name.to_s.match "(.+)_que_mais_vendeu_por_(.+)"
+        if matcher 
+            tipo = matcher[1]
+            campo = matcher[2].to_sym
+            que_mais_vendeu_por(tipo, &campo)
+        else
+            super
+        end
     end
-
-    def livro_que_mais_vendeu_por_ano
-        livro_que_mais_vendeu_por("livro", &:ano)
+    # diz se existe um metodo
+    def respond_to?(name)
+        name.to_s.match "(.+)_que_mais_vendeu_por_(.+)" || super 
     end
 
-    def livro_que_mais_vendeu_por_editora
-        livro_que_mais_vendeu_por("livro", &:editora)
-    end
-
-    # ---------- REVISTA -----------
-
-    def revista_que_mais_vendeu_por_titulo
-        livro_que_mais_vendeu_por("revista", &:titulo)
-    end
-
-    def revista_que_mais_vendeu_por_ano
-        livro_que_mais_vendeu_por("revista", &:ano)
-    end
-
-    def revista_que_mais_vendeu_por_editora
-        livro_que_mais_vendeu_por("revista", &:editora)
-    end
 
     # -------------------------------
 
@@ -85,5 +63,28 @@ class Estoque
 
     def length
         @livros.length
+    end
+
+
+
+
+
+
+
+
+
+
+    private
+
+    # conta a quantidade de ocorrencia de uma valor em um array de string
+    def quantidade_de_vendas_por(produto, &campo)
+        @vendas.count {|venda| campo.call(venda) == campo.call(produto)} 
+    end
+    
+    # reorganiza o array de livro e retorna o ultimo
+    def que_mais_vendeu_por(tipo, &campo)
+        @vendas.select {|l| l.tipo == tipo}.sort {|produto1,produto2|
+            quantidade_de_vendas_por(produto1, &campo) <=> 
+                quantidade_de_vendas_por(produto2, &campo)}.last
     end
 end
--- sistema.rb
+++ sistema.rb
@@ -2,20 +2,27 @@ require_relative "livro"
 require_relative "float_override"
 require_relative "estoque"
 
- 
+ # livros
  livro_algoritmos = Livro.new("Algoritmos", 100, 1998, true, "", "livro")
  livro_arquitetura = Livro.new("Introdução a Arquitetura e Design de Software", 70, 2011, true, "", "livro")
  livro_programmer = Livro.new("The Pragramtic Programmer", 100, 1999, true, "", "livro")
  livro_ruby = Livro.new("Programming Ruby", 100, 2004, true, "", "livro")
+
+ # revistas
  revistona = Livro.new("Revista de Ruby", 10, 2012, true, "Revista", "revista")
 
+ # e-book
+ ebook_arquitetura = Livro.new("Revista de Ruby", 10, 2012, true, "", "ebook")
  
  estoque = Estoque.new
- estoque << livro_algoritmos << livro_algoritmos << livro_ruby << livro_programmer << livro_arquitetura << livro_ruby << livro_ruby << revistona << revistona
+ estoque << livro_algoritmos << livro_algoritmos << livro_ruby << livro_programmer << livro_arquitetura << livro_ruby << livro_ruby << revistona << revistona << ebook_arquitetura
  estoque.vende livro_ruby
  estoque.vende livro_algoritmos
  estoque.vende livro_algoritmos
  estoque.vende revistona
+ estoque.vende ebook_arquitetura
 
  puts estoque.livro_que_mais_vendeu_por_titulo.titulo
  puts estoque.revista_que_mais_vendeu_por_titulo.titulo
+ puts estoque.ebook_que_mais_vendeu_por_titulo.titulo
+ puts estoque.respond_to?(:ebook_que_mais_vendeu_por_titulo)
